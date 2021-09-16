36461ae935f9d7674e0fe14100e7cda70bd4ea10
kiki3700 <5575492q@gmail.com>
2021-09-16 22:14:43 +0900

    prepare refactoring

---
+++ src/test/java/util/HttpRequestTest.java
@@ -0,0 +1,33 @@
+package util;
+
+import java.io.*;
+
+
+import org.junit.*;
+
+public class HttpRequestTest {
+	private String testDirectory = "./src/test/resources/";
+	
+	@Test
+	public void request_GET() throws Exception {
+		InputStream in = new FileInputStream(new File(testDirectory+"Http_GET.txt"));
+		HttpRequest request = new HttpRequest(in);
+		
+		assertEquals("GET", request.getMthod());
+		assertEquals("/user/create", request.getPath());
+		assertEquals("keep-alive", request.getHeader("Connection"));
+		assertEquals("javajigi", request.getParameter("userId"));
+		
+	}
+	
+	@Test
+	public void request_POST() throws Exception{
+		InputStream in = new FileInputStream(new File(testDirectory+"Http_POST.txt"));
+		HttpRequest request = new HttpRequest(in);
+		
+		assertEquals("POST", request.getMethod());
+		assertEquals("/user/create", request.getPath());
+		assertEquals("keep-alive", request.getHeader("Connection"));
+		assertEquals("javajigi", request.getParameter("userId"));
+	}
+}
